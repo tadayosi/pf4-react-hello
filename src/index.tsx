f609b510752fdff4897cd5e7e8c032c2a434dfe5
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2018-12-17 19:59:50 +0900

    add default navigation

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import '@patternfly/react-core/dist/styles/base.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
